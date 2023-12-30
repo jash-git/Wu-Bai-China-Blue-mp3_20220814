d1111bda7eda4eb2ce9a0092754d77f2d171c36c
jash.liao <jash.liao@gmail.com>
2023-12-30 08:56:58 +0800

    add 202312300856

--- 123.bat
+++ 123.bat
@@ -1,5 +1,13 @@
-set inputfile="videoplayback.mp4"
-
-set outputfile="videoplayback.mp3"
-
-ffmpeg.exe -i %inputfile% -acodec libmp3lame -ar 44100 -ab 128k %outputfile%
+ffmpeg.exe -i 01.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/01.mp3
+ffmpeg.exe -i 02.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/02.mp3
+ffmpeg.exe -i 03.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/03.mp3
+ffmpeg.exe -i 04.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/04.mp3
+ffmpeg.exe -i 05.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/05.mp3
+ffmpeg.exe -i 06.mp4 -acodec libmp3lame -ar 44100 -ab 128k ./mp3/06.mp3
+pause
+del 01.mp4
+del 02.mp4
+del 03.mp4
+del 04.mp4
+del 05.mp4
+del 06.mp4
